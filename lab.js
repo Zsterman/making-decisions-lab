15948b3c0d6818b26013c06fea4df9bd75d94812
Zachary Sterman <zacharysterman1992@gmail.com>
2021-09-21 22:18:40 -0400

    intermediate update

--- lab.js
+++ lab.js
@@ -160,13 +160,23 @@ else if (score < 60){
 */
 
 //CODE HERE
+var changeMyMind = true
+var changeMyMind = true;{
+ console.log('false') 
+}
+
+changeMyMind = !!changeMyMind
+console.log(changeMyMind)
+
+
 
 
 
 // Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
 
 //CODE HERE
-
+changeMyMind = !!changeMyMind
+console.log(changeMyMind)
 
 
 
@@ -181,6 +191,10 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 */
 
 //Code Here
+var someNum = myFavoriteNumbers[5];{
+  console.log(someNum)
+}
+
 
 
 
@@ -188,7 +202,13 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 // Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
 //Code Here
-
+if (myFavoriteNumbers.length < 7){
+  console.log('There are not enough elements in the array')
+}
+if (myFavoriteNumbers.length >= 7){
+  someNum = myFavoriteNumbers[6];
+  console.log(someNum);
+} 
 
 ////////// PROBLEM 11 //////////
 
@@ -199,7 +219,12 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
 
 //Code Here
-
+for(let i = 0; i < listOfNumbers.length; i++){
+  if(listOfNumbers[i]%3==0){
+    console.log(listOfNumbers[i])
+  }
+ 
+ }
 
 ////////// PROBLEM 12 //////////
 // Do not edit the code below.
@@ -211,6 +236,13 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+// for (let i = letters.length - 1; i >= 0; i--) {
+//   console.log(letters[i]);}
+
+letters.reverse()
+  console.log(letters);
+
+
 
 
 ////////// Advanced Problems //////////
